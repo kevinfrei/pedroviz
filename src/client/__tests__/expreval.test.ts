ee05b9a1599e6bbba61c583c6122c4e888a6f1ba
Kevin Frei <kevinfrei@hotmail.com>
2026-08-30 07:15:11 -0700

    Expanding testing of the Evaluator. (#9)

--- src/client/__tests__/expreval.test.ts
+++ src/client/__tests__/expreval.test.ts
@@ -3,11 +3,19 @@
 import { describe, expect, test } from 'bun:test';
 
 import { EmptyParsedClass } from '../../CodeTypeCheck';
-import { ValueName } from '../../CodeTypes';
+import { ValueName, ValueRef } from '../../CodeTypes';
+import { ConcreteHeadingType } from '../ConcreteEvalTypes';
 import {
-  /*calcValue,*/ GetValueAsString,
+  calcBezierRef,
+  calcFacing,
+  calcPoseRef,
+  calcValue,
+  calcValueRef,
+  GetValueAsString,
   readConstant,
 } from '../ExpressionEval';
+import { GetNameLookup, MakeFileIndex } from '../state/IndexedFile';
+import { fullParsedClass, testDatabase } from './testpaths.input';
 
 describe('Expression Evaluation', () => {
   test('Constants', () => {
@@ -21,11 +29,45 @@ describe('Expression Evaluation', () => {
     expect(GetValueAsString({ double: 123.456 })).toEqual('123.46');
     expect(GetValueAsString('varName' as ValueName)).toEqual('varName');
   });
-  // test.skip('calcValue', () => {
-  //   expect(calcValue({ int: 123 }, EmptyParsedClass)).toEqual(123);
-  //   expect(calcValue({ double: 12.3 }, EmptyParsedClass)).toEqual(12.3);
-  //   expect(calcValue({ radians: { double: 180 } }, EmptyParsedClass)).toEqual(
-  //     3.141592653589793,
-  //   );
-  // });
+  test('calcValue', () => {
+    expect(calcValue({ int: 123 }, EmptyParsedClass)).toEqual(123);
+    expect(calcValue({ double: 12.3 }, EmptyParsedClass)).toEqual(12.3);
+    expect(calcValue({ radians: { double: 180 } }, EmptyParsedClass)).toEqual(
+      3.141592653589793,
+    );
+  });
+  test('general calculationsd', () => {
+    const index = MakeFileIndex(fullParsedClass);
+    const lkup = GetNameLookup();
+    lkup.setDb(testDatabase);
+    const val = calcValueRef('val2' as ValueRef, fullParsedClass);
+    expect(val).toEqual(2.5);
+    const pose = calcPoseRef('pose1', fullParsedClass);
+    expect(pose).toEqual({ x: 2.5, y: 1 });
+    const pi = 'Math.PI' as ValueName;
+    const piVal = calcValueRef(pi, fullParsedClass);
+    expect(piVal).toEqual(3.141592653589793);
+    const poseHeading = calcPoseRef('pose3', fullParsedClass);
+    expect(poseHeading).toEqual({ x: 1, y: 2.5, h: 1.5707963267948966 });
+    const poseInder = calcPoseRef('pose4', fullParsedClass);
+    expect(poseInder).toEqual(pose);
+    const line = calcBezierRef('bez1', fullParsedClass);
+    expect(line).toEqual([
+      { x: 2.5, y: 1 },
+      { x: 2.5, y: 1, h: 1.0471975511965976 },
+    ]);
+    const curve = calcBezierRef('bez2', fullParsedClass);
+    expect(curve).toEqual([
+      { x: 1, y: 1 },
+      { x: 2.5, y: 1 },
+      { x: 2.5, y: 1, h: 1.0471975511965976 },
+    ]);
+    const pc = index.namedPathChains.get('pc2');
+    expect(pc).toBeDefined();
+    const heading = calcFacing(pc!.heading, fullParsedClass);
+    expect(heading).toEqual({
+      type: ConcreteHeadingType.Constant,
+      heading: 1.5707963267948966,
+    });
+  });
 });
